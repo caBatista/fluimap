b1dc1a73c7a9eaa4ad30917f7d2ce280431de433
Eduardo Rosa <eduardorosa10000@gmail.com>
2025-04-04 20:07:09 -0300

    US04 e US07 prontas(Criar e deletar times)

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -16,6 +16,7 @@ interface CardsProps {
   button1?: string;
   button2?: string;
   onOpenModal?: () => void;
+  onDelete?: () => void;
 }
 
 export function Cards({
@@ -24,33 +25,39 @@ export function Cards({
   button1,
   button2,
   onOpenModal,
+  onDelete,
 }: CardsProps) {
   return (
     <Card className="flex h-[200px] w-[350px] flex-col rounded-lg border-0 shadow-lg">
       <CardHeader className="flex h-[60px] items-center justify-between p-0">
-        {/* CardTitle à esquerda */}
         <div className="flex w-full items-center justify-between">
-          {/* Título à esquerda */}
           <CardTitle className="ml-4 mt-2 text-left text-xl">{name}</CardTitle>
 
-          {/* Botões à direita */}
           <div className="mr-2 flex gap-2">
-            <button className="flex items-center gap-2 rounded-md bg-white px-2 py-2 text-black dark:bg-gray-950 dark:text-white">
+            <button
+              className={
+                "flex items-center gap-2 rounded-md bg-white px-2 py-2 text-black dark:bg-gray-950 dark:text-white"
+              }
+              onClick={() => console.log("Editar", name)}
+            >
               <PencilIcon className="h-5 w-5 text-gray-800 dark:text-white" />
             </button>
-            <button className="flex items-center gap-2 rounded-md bg-white px-2 py-2 text-black dark:bg-gray-950 dark:text-white">
+            <button
+              className={
+                "flex items-center gap-2 rounded-md bg-white px-2 py-2 text-black dark:bg-gray-950 dark:text-white"
+              }
+              onClick={onDelete}
+            >
               <TrashIcon className="h-5 w-5 text-black dark:text-white" />
             </button>
           </div>
         </div>
       </CardHeader>
 
-      {/* CardDescription - Ajuste de margem esquerda (pode ser ajustado conforme necessário) */}
       <div className="mb-4 ml-6 flex flex-1 items-center justify-start">
         <CardDescription>{description}</CardDescription>
       </div>
 
-      {/* CardFooter - Aumentar o espaçamento inferior com margin */}
       <CardFooter className="mb-2 mt-auto flex justify-center gap-x-2">
         <Button className="rounded-md border border-black bg-white px-3 py-1 text-sm text-black">
           {button1}
--- src/components/modal/teamModal.tsx
+++ src/components/modal/teamModal.tsx
@@ -129,7 +129,7 @@ export function TeamModal({ isOpen, onClose, onSubmit }: TeamModalProps) {
           <button
             type="submit"
             className="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
-            onSubmit={handleSubmit}
+            onClick={handleSubmit}
           >
             Salvar
           </button>
