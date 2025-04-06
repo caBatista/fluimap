7fb2c8230f35535c4d4601ab219d7940d8f4a18e
MarcusQuirino <marcusfilipus@gmail.com>
2025-04-06 17:23:31 -0300

    fix lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import { PostForm } from '@/components/post-form';
-import { PostList } from '@/components/post-list';
-import dbConnect from '@/server/db';
-import { auth, currentUser } from '@clerk/nextjs/server';
-import User from '@/models/User';
+import { PostForm } from "@/components/post-form";
+import { PostList } from "@/components/post-list";
+import dbConnect from "@/server/db";
+import { auth, currentUser } from "@clerk/nextjs/server";
+// import User from "@/models/User";
 
-async function createUser(clerkId: string, name: string | null) {
-  const user = await User.findOne({ clerkId });
-  if (!user) {
-    await User.create({ clerkId, name });
-  }
-}
+// async function createUser(clerkId: string, name: string | null) {
+//   const user = await User.findOne({ clerkId });
+//   if (!user) {
+//     await User.create({ clerkId, name });
+//   }
+// }
 
 export default async function Home() {
   await dbConnect();
@@ -22,12 +22,12 @@ export default async function Home() {
 
   if (user === null) return redirectToSignIn();
 
-  async function createUser(clerkId: string, name: string | null) {
-    const user = await User.findOne({ clerkId });
-    if (!user && name) {
-      await User.create({ clerkId, name });
-    }
-  }
+  // async function createUser(clerkId: string, name: string | null) {
+  //   const user = await User.findOne({ clerkId });
+  //   if (!user && name) {
+  //     await User.create({ clerkId, name });
+  //   }
+  // }
 
   return (
     <main className="container mx-auto flex flex-col items-center">
