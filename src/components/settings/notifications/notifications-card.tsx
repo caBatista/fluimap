c4aac5b8e837e8a0bc853e8e653949141ab4f913
Gustavo Caldeira de Mesquita <g.caldeira@edu.pucrs.br>
2025-06-06 20:41:06 -0300

    ajuste de erro

--- src/components/settings/notifications/notifications-card.tsx
+++ src/components/settings/notifications/notifications-card.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Switch } from '@/components/ui/switch';
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { useForm } from 'react-hook-form';
@@ -32,22 +31,9 @@ export function NotificationsCard({ initial }: { initial: NotificationsData }) {
         <CardTitle>Configurações de notificação</CardTitle>
       </CardHeader>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <CardContent className="grid gap-4">
-          <div className="flex items-center justify-between">
-            <span>Notificar sobre novos formulários</span>
-            <Switch {...register('emailSurveys')} />
-          </div>
-          <div className="flex items-center justify-between">
-            <span>Notificar sobre novos relatórios</span>
-            <Switch {...register('emailReports')} />
-          </div>
-        </CardContent>
-
-        <CardFooter>
-          <Button disabled={formState.isSubmitting}>Salvar</Button>
-        </CardFooter>
-      </form>
+      <CardFooter>
+        <Button disabled={formState.isSubmitting}>Salvar</Button>
+      </CardFooter>
     </Card>
   );
 }
