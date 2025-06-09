f6e082c5eea842e3b68bb21ca686cfb39794d130
gabriel-ramos56 <gabriel.ramos.0800@gmail.com>
2025-06-08 22:55:16 -0300

    criação do header do dashboard admin

---
+++ src/components/dashboard/dashboard-admin-header.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export function DashboardAdminHeader() {
+  return (
+    <div className="flex items-center justify-between">
+      <div>
+        <h1 className=" text-2xl font-bold">Dashboard Administrador</h1>
+        <p className="mt-1 text-sm text-[hsl(var(--muted-foreground))]">
+          Acompanhe o funcionamento da plataforma e tenha controle sobre os
+          usuários
+        </p>
+      </div>
+    </div>
+  );
+}
